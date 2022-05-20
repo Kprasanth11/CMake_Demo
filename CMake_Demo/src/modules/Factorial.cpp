c1a17433c5bc66157de6c1e9d790574b93dfaa2f
K Prasanth <105642546+Kprasanth11@users.noreply.github.com>
2022-05-20 10:32:24 +0530

    Add files via upload

---
+++ CMake_Demo/src/modules/Factorial.cpp
@@ -0,0 +1,9 @@
+#include "Factorial.hpp"
+
+// function to find factorial of given number
+unsigned int factorial(unsigned int n)
+{
+    if (n == 0)
+        return 1;
+    return n * factorial(n - 1);
+}
